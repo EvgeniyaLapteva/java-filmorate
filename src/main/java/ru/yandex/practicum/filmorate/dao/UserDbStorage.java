6514cfb20426cdb2761273c36ccbff602aaa7de4
EvgeniyaLapteva <evgeniyalaptev4@yandex.ru>
2023-05-01 05:55:51 +0700

    Improve UserDbStorage

--- src/main/java/ru/yandex/practicum/filmorate/dao/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/UserDbStorage.java
@@ -1,57 +1,68 @@
 package ru.yandex.practicum.filmorate.dao;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
-import java.sql.Date;
-import java.sql.PreparedStatement;
+import java.sql.*;
 import java.util.List;
-import java.util.Objects;
 
-@Component
+@Repository
 @Primary
+@RequiredArgsConstructor
 public class UserDbStorage implements UserStorage {
 
     private final JdbcTemplate jdbcTemplate;
-
-    public UserDbStorage(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
 
     @Override
     public User createUser(User user) {
         String sql = "insert into users (email, login, name, birthday) values (?, ?, ?, ?)";
         KeyHolder keyHolder = new GeneratedKeyHolder();
         jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(sql);
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, user.getEmail());
             ps.setString(2, user.getLogin());
             ps.setString(3, user.getName());
             ps.setDate(4, Date.valueOf(user.getBirthday()));
             return ps;
         }, keyHolder);
-        user.setId(Objects.requireNonNull(keyHolder.getKey()).intValue());
+        user.setId(keyHolder.getKey().intValue());
+        return user;
+    }
+
+
+    @Override
+    public User updateUser(User user) {
+        String sql = "update users set email=?, login=?, name=?, birthday=? where user_id=?";
+        jdbcTemplate.update(sql, user.getEmail(), user.getLogin(), user.getName(), user.getBirthday(), user.getId());
         return user;
     }
 
     @Override
-    public User updateUser(User user) {
-        return null;
-    }
-
-    @Override
     public List<User> getAllUsers() {
-        return null;
+        String sql = "select * from users";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> mapRowToUser(rs));
     }
 
     @Override
     public User getUserById(int userId) {
-        return null;
+        String sql = "select * from users where user_id = ?";
+        return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> mapRowToUser(rs), userId);
     }
+
+    private User mapRowToUser(ResultSet resultSet) throws SQLException {
+        return User.builder()
+                .id(resultSet.getInt("user_id"))
+                .email(resultSet.getString("email"))
+                .login(resultSet.getString("login"))
+                .name(resultSet.getString("name"))
+                .birthday(resultSet.getDate("birthday").toLocalDate()).build();
+    }
+
+
 }
